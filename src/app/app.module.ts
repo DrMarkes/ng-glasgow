48e4d8081a8ece050f6cca2902ced8d910616c87
Андрей Марков <user.name>
2017-10-11 16:41:51 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ResponseCardComponent } from './response-card/response-card.component';
+import {MaterialDesignModule} from './material-design/material-design.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResponseCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialDesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
